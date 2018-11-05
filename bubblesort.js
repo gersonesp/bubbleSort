5344f82c8a21fd73915eb80cdc24c2bfb4fae2a5
Jonathan Ciccarella <JonCiccarella@gmail.com>
2018-11-05 12:31:52 -0500

    tried a for loop 2

--- bubblesort.js
+++ bubblesort.js
@@ -1,12 +1,16 @@
 function swap(item1, item2) {
-
+    //if item
 }
 
 function bubbleSort(array) {
-
     /* your code here */
+    let sortedArr = [];
     if (array.length <= 1) return array;
 
+    for (let i = 0; i < array.length; i++) {
+        sortedArr.push(swap(array[i], array[i + 1]));
+    }
 
 
+    return sortedArr;
 }
